f1d545c9d7034a6f1cb96edcdf43a5831674b45f
RayCC51 <4q26dd@gmail.com>
2025-02-15 11:30:13 +0900

    feat: clear table

--- export.js
+++ export.js
@@ -8,7 +8,7 @@ document.getElementById("fileOutput").addEventListener("click", function () {
   }
 
   // 이중 배열 초기화
-  const array2D = [];
+  let array2D = [];
 
   // 테이블의 각 행을 반복
   for (let i = 0; i < table.rows.length; i++) {
@@ -31,6 +31,10 @@ document.getElementById("fileOutput").addEventListener("click", function () {
     // 행 배열을 이중 배열에 추가
     array2D.push(rowArray);
   }
+
+  // remove empty last row column
+  array2D = removeEmptyLastColumn(array2D);
+  array2D = removeEmptyLastRow(array2D);
 
   // 결과 출력
   //   console.log(array2D);
@@ -61,4 +65,30 @@ function downloadCSV(data) {
   document.body.appendChild(link);
   link.click();
   document.body.removeChild(link);
+}
+
+function removeEmptyLastRow(data) {
+  // 마지막 행이 빈 문자열로만 이루어져 있는지 확인
+  const lastRowIndex = data.length - 1; // 마지막 행의 인덱스
+  if (
+    lastRowIndex >= 0 &&
+    data[lastRowIndex].every((cell) => cell.trim() === "")
+  ) {
+    return data.slice(0, lastRowIndex); // 마지막 행을 제외한 새로운 배열 반환
+  }
+  return data; // 마지막 행이 비어있지 않으면 원본 데이터 반환
+}
+
+function removeEmptyLastColumn(data) {
+  // 마지막 열이 빈 문자열로만 이루어진 경우 해당 열을 삭제
+  const lastColumnIndex = data[0].length - 1; // 마지막 열의 인덱스
+  const isLastColumnEmpty = data.every(
+    (row) => row[lastColumnIndex].trim() === ""
+  ); // 모든 행의 마지막 열이 빈 문자열인지 확인
+
+  if (isLastColumnEmpty) {
+    return data.map((row) => row.slice(0, lastColumnIndex)); // 마지막 열을 제외한 새로운 배열 반환
+  }
+
+  return data; // 마지막 열이 비어있지 않으면 원본 데이터 반환
 }
--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// hide text button
 document.getElementById("hideText").addEventListener("click", function () {
   const tds = document.querySelectorAll("td");
 
@@ -15,6 +16,50 @@ document.getElementById("hideText").addEventListener("click", function () {
   });
 });
 
+// clear table
+document.getElementById("clearTable").addEventListener("click", function () {
+  const initFile = "init.csv"; // 초기화할 CSV 파일 경로
+
+  // fetch를 사용하여 init.csv 파일을 읽기
+  fetch(initFile)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.text(); // 파일 내용을 텍스트로 변환
+    })
+    .then((csvData) => {
+      const parsedData = CSVToArray(csvData); // CSV 파싱
+      generateTable(parsedData); // 테이블 생성 함수 호출
+    })
+    .catch((error) => {
+      console.error("Error fetching the CSV file:", error);
+    });
+});
+
+// 페이지가 로드될 때 sample.csv 파일을 자동으로 읽어오기
+window.onload = function () {
+  const initFile = "sample.csv"; // 초기화할 CSV 파일 경로
+
+  // fetch를 사용하여 sample.csv 파일을 읽기
+  fetch(initFile)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.text(); // 파일 내용을 텍스트로 변환
+    })
+    .then((csvData) => {
+      const parsedData = CSVToArray(csvData); // CSV 파싱
+      const filteredData = removeEmptyLastRow(parsedData);
+      generateTable(filteredData); // 테이블 생성 함수 호출
+    })
+    .catch((error) => {
+      console.error("Error fetching the CSV file:", error);
+    });
+};
+
+// open file
 document
   .getElementById("fileInput")
   .addEventListener("change", function (event) {
@@ -29,7 +74,9 @@ document
       reader.onload = function (e) {
         const csvData = e.target.result; // 파일 내용 가져오기
         const parsedData = CSVToArray(csvData); // CSV 파싱
-        generateTable(parsedData); // 테이블 생성 함수 호출
+        // generateTable(parsedData); // 테이블 생성 함수 호출
+        const filteredData = removeEmptyLastRow(parsedData);
+        generateTable(filteredData); // 테이블 생성 함수 호출
       };
 
       // 파일을 텍스트로 읽기
@@ -39,6 +86,18 @@ document
     }
   });
 
+function removeEmptyLastRow(data) {
+  // 마지막 행이 빈 문자열로만 이루어져 있는지 확인
+  const lastRowIndex = data.length - 1; // 마지막 행의 인덱스
+  if (
+    lastRowIndex >= 0 &&
+    data[lastRowIndex].every((cell) => cell.trim() === "")
+  ) {
+    return data.slice(0, lastRowIndex); // 마지막 행을 제외한 새로운 배열 반환
+  }
+  return data; // 마지막 행이 비어있지 않으면 원본 데이터 반환
+}
+
 // 2차원 배열을 HTML 테이블로 변환하는 함수
 function generateTable(data) {
   const tableContainer = document.getElementById("tableContainer");
@@ -46,6 +105,9 @@ function generateTable(data) {
 
   const table = document.createElement("table");
   table.id = "colorTable";
+
+  // 최대 열 수 계산
+  const maxColumns = Math.max(...data.map((row) => row.length));
 
   data.forEach((row) => {
     const tr = document.createElement("tr"); // 행 생성
@@ -63,6 +125,15 @@ function generateTable(data) {
 
       tr.appendChild(td); // 행에 셀 추가
     });
+
+    // 빈 셀 추가
+    const emptyCellsCount = maxColumns - row.length;
+    for (let i = 0; i < emptyCellsCount; i++) {
+      const emptyTd = document.createElement("td"); // 빈 셀 생성
+      emptyTd.classList.add("colorBox");
+      tr.appendChild(emptyTd); // 행에 빈 셀 추가
+    }
+
     table.appendChild(tr); // 테이블에 행 추가
   });
 
@@ -78,6 +149,7 @@ function addInnerHTML(obj, code) {
     }</p><p class="colorCode">${code.toUpperCase()}</p>`;
   }
 }
+
 function addClickEventToCells() {
   // td clickable
   // 모든 td 요소 선택

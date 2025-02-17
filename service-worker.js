bb3b7b6e2eab266d7f9522ec17ace17e59c62358
RayCC51 <4q26dd@gmail.com>
2025-02-17 10:20:37 +0900

    fix: change scope

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,21 @@
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/color-collector/service-worker.js")
+      .then((registration) => {
+        console.log(
+          "Service Worker registered with scope:",
+          registration.scope
+        );
+      })
+      .catch((error) => {
+        console.error("Service Worker registration failed:", error);
+      });
+  });
+}
+
 self.addEventListener("install", (event) => {
+  console.log("Service Worker installing...");
   event.waitUntil(
     caches.open("my-cache").then((cache) => {
       return cache.addAll([
@@ -23,6 +40,7 @@ self.addEventListener("install", (event) => {
 });
 
 self.addEventListener("fetch", (event) => {
+  console.log("Fetching:", event.request.url);
   event.respondWith(
     caches.match(event.request).then((response) => {
       return response || fetch(event.request);
